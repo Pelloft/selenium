b220e44c3a4f70fbd5fcc5a392f3cddbbcd011ad
Pelloft <anthony-flores_@hotmail.com>
2026-04-01 01:54:14 -0500

    Validación y navegación por la web

--- src/main/java/Locators/locators.java
+++ src/main/java/Locators/locators.java
@@ -5,6 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 
 public class locators {
     public static void main(String [] args){
@@ -23,29 +28,60 @@ public class locators {
         String usuario = "standard_user";
         String password = "secret_sauce";
 
-        //Localizador by ID
-        WebElement usuarioInput = driver.findElement(By.id("user-name"));
-        WebElement passwordInput = driver.findElement(By.id("password"));
-        //WebElement loginBtn = driver.findElement(By.id("login-button"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        //Locator by Class Name
-        //WebElement loginBtn = driver.findElement(By.className("btn_action"));
+        try{
+            //Localizador by ID
+            WebElement usuarioInput = driver.findElement(By.id("user-name"));
+            WebElement passwordInput = driver.findElement(By.id("password"));
+            //WebElement loginBtn = driver.findElement(By.id("login-button"));
 
-        //Locator by name
-        //WebElement loginBtn = driver.findElement(By.name("login-button"));
+            //Locator by Class Name
+            //WebElement loginBtn = driver.findElement(By.className("btn_action"));
 
-        //Locator XPATH
-        WebElement loginBtn = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
+            //Locator by name
+            //WebElement loginBtn = driver.findElement(By.name("login-button"));
 
-        //Locator by CSS selector
-        //WebElement loginBtn = driver.findElement(By.cssSelector());
+            //Locator XPATH
+            WebElement loginBtn = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
 
-        //Loggin
-        usuarioInput.sendKeys(usuario);
-        passwordInput.sendKeys(password);
-        loginBtn.click();
+            //Locator by CSS selector
+            //WebElement loginBtn = driver.findElement(By.cssSelector());
 
-        //driver.quit();
+            //Loggin
+            usuarioInput.sendKeys(usuario);
+            passwordInput.sendKeys(password);
+            loginBtn.click();
 
+
+            WebElement inventory = wait.until(
+                    ExpectedConditions.visibilityOfElementLocated(By.id("inventory_container"))
+            );
+
+            System.out.println("Login");
+
+            // Ejemplo: hacer clic en un producto
+            WebElement producto = driver.findElement(By.xpath("//div[text()='Sauce Labs Backpack']"));
+            producto.click();
+
+            Thread.sleep(2000); // solo para visualizar
+
+            // Agregar al carrito
+            WebElement addToCart = driver.findElement(By.xpath("//button[text()='ADD TO CART']"));
+            addToCart.click();
+
+            Thread.sleep(2000);
+
+            // Ir al carrito
+            WebElement carrito = driver.findElement(By.id("shopping_cart_container"));
+            carrito.click();
+
+            Thread.sleep(3000);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
     }
 }
